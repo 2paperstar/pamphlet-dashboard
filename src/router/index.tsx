0ff7d29b8abb03856776bce6d85ade7f8eedd503
2paperstar <lee@paperst.ar>
2023-04-30 02:20:58 +0900

    feat: booth info ui

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import BoothSummary from '../pages/BoothSummary';
+import BoothInfo from '../pages/BoothInfo';
 
 const Router = () => {
   return (
@@ -9,7 +10,7 @@ const Router = () => {
         <Route path="/" element={<MainLayout />}>
           <Route path="booth">
             <Route path="summary" element={<BoothSummary />} />
-            <Route path="info" element={<div>부스 정보 정리</div>} />
+            <Route path="info" element={<BoothInfo />} />
             <Route path="detail" element={<div>부스 상세 설명</div>} />
           </Route>
           <Route path="event" element={<div>이벤트 관리</div>} />
